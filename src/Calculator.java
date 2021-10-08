47e406473ebe075cab9cade3a00bf6def60f366e
bstrobl <00812893@edu.campus02.at>
2021-10-08 21:27:03 +0200

    methoden definiert

--- src/Calculator.java
+++ src/Calculator.java
@@ -14,5 +14,47 @@ public class Calculator {
         System.out.println(d);
         System.out.println(f);
 
+        System.out.println("----------------------------------------------------------------------------");
+
+        int result = add(5,3);
+        returnResult(result);
+
+        int result1 = sub(5,3);
+        returnResult(result1);
+
+        int result2 = div(5,3);
+        returnResult(result2);
+
+        int result3 = mul(5,3);
+        returnResult(result3);
+
+        int result4 = mod(5,3);
+        returnResult(result4);
+
+    }
+
+    public static int add(int a, int b){
+        return a + b;
+    }
+
+    public static int sub(int a, int b){
+        return a - b;
+    }
+
+    public static int div(int a, int b){
+        return a / b;
+    }
+
+    public static int mul(int a, int b){
+        return a * b;
+    }
+
+    public static int mod(int a, int b) {
+        return a % b;
+
+    }
+
+    public static void returnResult(int result){
+        System.out.println(result);
     }
 }
