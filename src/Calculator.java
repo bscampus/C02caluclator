0f6013e363910de6d956a31c623dc20aabbdaef6
bstrobl <00812893@edu.campus02.at>
2021-10-08 19:58:44 +0200

    variablen deklariert

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,5 +2,20 @@ public class Calculator {
 
     public static void main(String[] args) {
 
+        int a = 1;
+        double b = 2141;
+        boolean c = true;
+        char d = 'D';
+        String f = "HelloWorld";
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+
+        
+
+
     }
 }
